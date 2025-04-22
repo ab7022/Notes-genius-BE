6dc128ca735ddd8ed26efee9d9b6cd9e2e22b67a
Abdul Bayees <bayees1@gmail.com>
2025-04-23 02:08:33 +0530

    updared port

--- index.js
+++ index.js
@@ -64,4 +64,6 @@ app.post('/summarize', async (req, res) => {
 })
 
 const PORT = process.env.PORT || 3000
-app.listen(3001)
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`)
+})
